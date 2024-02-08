288a2ad69001a6f743565264fd84998731ae3235
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-02-08 17:24:06 +0200

    Create Gamecontroller object

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,1 +1,21 @@
-//script
+function Gamecontroller(playerOneName = "Player One", playerTwoName = "Player Two") {
+  function Gameboard() {
+    const size = 3;
+    const board = Array(size*size);
+    console.log("it runs!!");
+    return board;
+  }
+
+  const board = Gameboard();
+
+  const players = [
+    {
+      name: playerOneName,
+      sign: "X"
+    },
+    {
+      name: playerTwoName,
+      sign: "O"
+    }
+  ];
+}
