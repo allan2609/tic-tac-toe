00e2598e1b6824d68d0e2a9b411bcce1ab90dea3
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-02-23 19:36:27 +0200

    Remove unnecessary eventlistener

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,72 +1,29 @@
 const Gamecontroller = (function () {
- 
-  function checkWinConditions() {
-    if (board[0] === "X" && board[1] === "X" && board[2] === "X") {
-      console.log("Game over. X wins.");
-    } else if (board[3] === "X" && board[4] === "X" && board[5] === "X") {
-      console.log("Game over. X wins.");
-    } else if (board[6] === "X" && board[7] === "X" && board[8] === "X") {
-      console.log("Game over. X wins.");
-    } else if (board[0] === "X" && board[3] === "X" && board[6] === "X") {
-      console.log("Game over. X wins.");
-    } else if (board[1] === "X" && board[4] === "X" && board[7] === "X") {
-      console.log("Game over. X wins.");
-    } else if (board[2] === "X" && board[5] === "X" && board[8] === "X") {
-      console.log("Game over. X wins.");
-    } else if (board[0] === "X" && board[4] === "X" && board[8] === "X") {
-      console.log("Game over. X wins.");
-    } else if (board[2] === "X" && board[4] === "X" && board[6] === "X") {
-      console.log("Game over. X wins.");
-    } else if (board[0] === "O" && board[1] === "O" && board[2] === "O") {
-      console.log("Game over. O wins.");
-    } else if (board[3] === "O" && board[4] === "O" && board[5] === "O") {
-      console.log("Game over. O wins.");
-    } else if (board[6] === "O" && board[7] === "O" && board[8] === "O") {
-      console.log("Game over. O wins.");
-    } else if (board[0] === "O" && board[3] === "O" && board[6] === "O") {
-      console.log("Game over. O wins.");
-    } else if (board[1] === "O" && board[4] === "O" && board[7] === "O") {
-      console.log("Game over. O wins.");
-    } else if (board[2] === "O" && board[5] === "O" && board[8] === "O") {
-      console.log("Game over. O wins.");
-    } else if (board[0] === "O" && board[4] === "O" && board[8] === "O") {
-      console.log("Game over. O wins.");
-    } else if (board[2] === "O" && board[4] === "O" && board[6] === "O") {
-      console.log("Game over. O wins.");
+  const board = Array(9);
+
+  const gameStatus = {
+    active: true,
+    toggleStatus: () => {
+      gameStatus.active = !gameStatus.active;
     }
-  }
- 
-  let marker;
- 
-  function getMarker() {  
-    if (marker == undefined || marker == "O") {
-      marker = "X";
-    } else if (marker == "X") {
-      marker = "O";
-    }
-    return marker;
+  };
+
+  function Player(name, marker) {
+    this.name = name;
+    this.marker = marker;
+    this.score = 0;
   }
 
-  function getIndex() {
-    const container = document.querySelector(".container");
-    for (let i = 0; i < board.length; i++) {
-      container.children[i].onclick = () => {
-        mark(i);
-      }
-    }
-  }
- 
-  const board = Array(9);
-  console.log(board);
- 
-  const mark = (index) => {
-    if (board[index] == undefined) {
-      board.splice(index, 1, getMarker());
-      checkWinConditions();
-      render();
-    } else {
-    console.log("invalid move");
-    }
+  const players = [
+    new Player("Player X", "X"),
+    new Player("Player O", "O"),
+  ];
+
+  let currentPlayer;
+
+  function getCurrentPlayer() {
+    currentPlayer != players[0] ? currentPlayer = players[0] : currentPlayer = players[1];
+    return currentPlayer;
   }
 
   function render() {
@@ -75,16 +32,106 @@ const Gamecontroller = (function () {
     while (container.hasChildNodes()) {
       container.removeChild(container.firstChild);
     }
- 
+
     for (let i = 0; i < board.length; i++) {
       const cell = document.createElement("div");
       container.appendChild(cell);
       cell.className = "cell";
       cell.textContent = board[i];
-      cell.addEventListener("click", getIndex);
+      cell.setAttribute("data-id", i);
     }
-  };
 
-  //return { mark }; 
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => cell.addEventListener("click", mark));
+  }
+
+  function mark(e) {
+    if (gameStatus.active) {
+      let index = e.currentTarget.dataset.id;
+      if (board[index] == undefined) {
+        board.splice(index, 1, getCurrentPlayer().marker);
+        render();
+        checkWinConditions();
+      } else {
+        console.log("invalid move");
+      }
+    }
+    if (!gameStatus.active) {
+      console.log("clicked after end of game");
+    }
+  }
+
+  function checkWinConditions() {
+    if (board[0] === "X" && board[1] === "X" && board[2] === "X") {
+      gameStatus.toggleStatus();
+      currentPlayer.score++; 
+      announceWinner();
+    } else if (board[3] === "X" && board[4] === "X" && board[5] === "X") {
+      gameStatus.toggleStatus();
+      currentPlayer.score++; 
+      announceWinner();
+    } else if (board[6] === "X" && board[7] === "X" && board[8] === "X") {
+      gameStatus.toggleStatus();
+      currentPlayer.score++; 
+      announceWinner();
+    } else if (board[0] === "X" && board[3] === "X" && board[6] === "X") {
+      gameStatus.toggleStatus();
+      currentPlayer.score++; 
+      announceWinner();
+    } else if (board[1] === "X" && board[4] === "X" && board[7] === "X") {
+      gameStatus.toggleStatus();
+      currentPlayer.score++; 
+      announceWinner();
+    } else if (board[2] === "X" && board[5] === "X" && board[8] === "X") {
+      gameStatus.toggleStatus();
+      currentPlayer.score++; 
+      announceWinner();
+    } else if (board[0] === "X" && board[4] === "X" && board[8] === "X") {
+      gameStatus.toggleStatus();
+      currentPlayer.score++; 
+      announceWinner();
+    } else if (board[2] === "X" && board[4] === "X" && board[6] === "X") {
+      gameStatus.toggleStatus();
+      currentPlayer.score++; 
+      announceWinner();
+    } else if (board[0] === "O" && board[1] === "O" && board[2] === "O") {
+      gameStatus.toggleStatus();
+      currentPlayer.score++; 
+      announceWinner();
+    } else if (board[3] === "O" && board[4] === "O" && board[5] === "O") {
+      gameStatus.toggleStatus();
+      currentPlayer.score++; 
+      announceWinner();
+    } else if (board[6] === "O" && board[7] === "O" && board[8] === "O") {
+      gameStatus.toggleStatus();
+      currentPlayer.score++; 
+      announceWinner();
+    } else if (board[0] === "O" && board[3] === "O" && board[6] === "O") {
+      gameStatus.toggleStatus();
+      currentPlayer.score++; 
+      announceWinner();
+    } else if (board[1] === "O" && board[4] === "O" && board[7] === "O") {
+      gameStatus.toggleStatus();
+      currentPlayer.score++; 
+      announceWinner();
+    } else if (board[2] === "O" && board[5] === "O" && board[8] === "O") {
+      gameStatus.toggleStatus();
+      currentPlayer.score++; 
+      announceWinner();
+    } else if (board[0] === "O" && board[4] === "O" && board[8] === "O") {
+      gameStatus.toggleStatus();
+      currentPlayer.score++; 
+      announceWinner();
+    } else if (board[2] === "O" && board[4] === "O" && board[6] === "O") {
+      gameStatus.toggleStatus();
+      currentPlayer.score++; 
+      announceWinner();
+    }
+  }
+
+  function announceWinner() {
+    alert("Game over. " + currentPlayer.name + " has won. Their score is " + currentPlayer.score);
+  }
+
   render();
 })();
