f60f4d397065209fa28b335e405bfcf63565e368
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-02-27 14:20:09 +0200

    Rewrite checkWinConditions

--- assets/javascript.js
+++ assets/javascript.js
@@ -59,49 +59,29 @@ const Gamecontroller = (function () {
   let winner;
 
   function checkWinConditions() {
-    if (board[0] == currentPlayer.marker && board[1] == currentPlayer.marker && board[2] == currentPlayer.marker) {
-      gameStatus.toggleStatus();
-      currentPlayer.score++;
-      winner = currentPlayer;
-      announceWinner();
-    } else if (board[3] == currentPlayer.marker && board[4] == currentPlayer.marker && board[5] == currentPlayer.marker) {
-      gameStatus.toggleStatus();
-      currentPlayer.score++;
-      winner = currentPlayer;
-      announceWinner();
-    } else if (board[6] == currentPlayer.marker && board[7] == currentPlayer.marker && board[8] == currentPlayer.marker) {
-      gameStatus.toggleStatus();
-      currentPlayer.score++;
-      winner = currentPlayer;
-      announceWinner();
-    } else if (board[0] == currentPlayer.marker && board[3] == currentPlayer.marker && board[6] == currentPlayer.marker) {
-      gameStatus.toggleStatus();
-      currentPlayer.score++;
-      winner = currentPlayer;
-      announceWinner();
-    } else if (board[1] == currentPlayer.marker && board[4] == currentPlayer.marker && board[7] == currentPlayer.marker) {
-      gameStatus.toggleStatus();
-      currentPlayer.score++;
-      winner = currentPlayer;
-      announceWinner();
-    } else if (board[2] == currentPlayer.marker && board[5] == currentPlayer.marker && board[8] == currentPlayer.marker) {
-      gameStatus.toggleStatus();
-      currentPlayer.score++;
-      winner = currentPlayer;
-      announceWinner();
-    } else if (board[0] == currentPlayer.marker && board[4] == currentPlayer.marker && board[8] == currentPlayer.marker) {
-      gameStatus.toggleStatus();
-      currentPlayer.score++;
-      winner = currentPlayer;
-      announceWinner();
-    } else if (board[2] == currentPlayer.marker && board[4] == currentPlayer.marker && board[6] == currentPlayer.marker) {
-      gameStatus.toggleStatus();
-      currentPlayer.score++;
-      winner = currentPlayer;
-      announceWinner();
-    } else if (!board.includes(undefined)) {
-      gameStatus.toggleStatus();
-      announceWinner();
+    const conditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < conditions.length; i++) {
+      const [a, b, c] = conditions[i];
+      if (board[a] == currentPlayer.marker && board[b] == currentPlayer.marker && board[c] == currentPlayer.marker) {
+        gameStatus.toggleStatus();
+        currentPlayer.score++;
+        winner = currentPlayer;
+        announceWinner();
+      } else if (!board.includes(undefined)) {
+        gameStatus.toggleStatus();
+        announceWinner();
+        return;
+      }
     }
   }
 
